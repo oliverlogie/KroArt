ce11f11b749e88ffe07bc42293af2301db687799
Oliver Kraus <oliver.s.kraus@gmail.com>
2020-12-08 16:50:23 +0100

    changes navbar

--- Website/includes/navbar.php
+++ Website/includes/navbar.php
@@ -12,14 +12,14 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </head>
         <body>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <nav class="navbar navbar-expand-lg navbar-light">
                 <a class="navbar-brand" href="#"><img class="logo" src="../img/logo_01.png" alt=""></a>
                 <a class="navbar-brand" href="#"><img class="logoMobile" src="../img/logo_01.png" alt=""></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse navbarItems" id="navbarSupportedContent">
-                    <ul class="navbar-nav navbarOwn ml-auto">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
                     <li>
                         <form class="form-inline my-2 my-lg-0 searchbar">
                         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
@@ -42,7 +42,7 @@
                         <a class="nav-link" href="#">contact</a>
                     </li>
                     <li class="nav-item collect">
-                        <a class="nav-link" href="#">collect <img class="navbarBag" src="../img/bag.png" alt=""></a>
+                        <a class="nav-link" href="#">collect <span class="navbarBag"><img src="../img/bag.png" alt=""></span></a>
                     </li>
                     </ul>
                 </div>
