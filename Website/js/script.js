852ecea8ad3fdae61e6fc9b5370c5c9ebf79ff2d
Erdzhan Alekov <erito31@gmail.com>
2020-12-11 11:35:43 +0100

    navbar done

---
+++ Website/js/script.js
@@ -0,0 +1,17 @@
+// function funtionBurgermenu(x) {
+// x.classList.toggle("change");
+// var x = document.getElementById("phoneMenu");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
