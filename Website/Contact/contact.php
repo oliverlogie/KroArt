caba449006661c4ee9572e2143d67ae95e92f755
Erdzhan Alekov <erito31@gmail.com>
2020-12-15 22:59:26 +0100

    intex.php - Home Page

--- Website/Contact/contact.php
+++ Website/Contact/contact.php
@@ -18,14 +18,8 @@
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2659.237651674816!2d16.360592749519714!3d48.202038226733954!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476d07622cbed4b1%3A0x65d6d1849097ff9!2sArtwork%20Coworking%20Space!5e0!3m2!1sbg!2sat!4v1607511435018!5m2!1sbg!2sat"  frameborder="0" style="border-radius:30px;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
         </div> 
     </main>
-    <footer>
-        <?php include_once('../includes/footer.php') ?>
-    </footer>
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>    
-    <script src="../js/script.js"></script>
+     <?php include_once('../includes/footer.php') ?>
+  
 
 </body>
 </html>
