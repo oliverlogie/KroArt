6bc5143b197c11393bfbb3547fdd2cad6422687d
oliverlogie <oliver.s.kraus@gmail.com>
2021-01-05 20:49:55 +0100

    updated artist

--- Website/artists/artists.php
+++ Website/artists/artists.php
@@ -5,22 +5,22 @@ $result_artist = $conn->query($sql_artist);
 <main>
   <div class="container text-center pt-5 pb-5">
       <h1>Artists</h1>
+      <div class="row artistsCard d-flex mt-5">
       <?php
         if(mysqli_num_rows($result_artist) > 0){
           $i=1;
         while($row = mysqli_fetch_array($result_artist)){
-          echo '<div class="row artistsCard d-flex mt-5">';
           echo '<div class="card border-0 col-lg-3 col-md-4 col-sm-6  text-left">';
           echo '<a href="../artist_Info/artist_info.php?id=' . $row['artist_id'] . '"><img class="card-img-top" src="../'. $row["picture"] .'" alt=""></a>';
           echo '<div class="card-body text-center">';
           echo '<a href="../artist_Info/artist_info.php?id=' . $row['artist_id'] . '"><h5 class="card-title">' . $row["name"] . '</h5></a>';
           echo '</div>';
           echo '</div>';
-          echo '</div>';
         }
         $i++;
       }
     ?>
+    </div>
   </div>
 </main>
 <?php include_once('../includes/footer.php') ?>
